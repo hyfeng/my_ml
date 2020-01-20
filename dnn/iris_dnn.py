3a6b6365701efde9dd1e24bdf104561d01282fbc
houyufeng <houyufeng@oppo.com>
2020-01-20 10:39:02 +0800

    dnn 参数

--- dnn/iris_dnn.py
+++ dnn/iris_dnn.py
@@ -19,51 +19,51 @@ import random
 import math
 from sklearn import datasets
 
-debug = True 
+debug = 1 
 
 espi = 0.000000001
 
 class Layer:
     def __init__(self, unit_num, feature_num, activation = sigmoid):
         self.unit_num = unit_num
-        self.input_num = feature_num
-        ##增加一个偏置
-        self.feature_num = feature_num + 1
+        self.feature_num = feature_num
+        if debug == 2:
+            print("{}".format((unit_num, self.feature_num)))
         self.w = np.random.uniform(-0.5, 0.5, (unit_num, self.feature_num))
+        self.bias = np.random.uniform(-0.5,0.5, (unit_num)).reshape((unit_num, 1))
         self.activate = activation
         ##学习率
         self.lam = 0.01
         self.x = None
 
     def print(self):
-        s = "{}".format(self.w)
+        s = "w:{}; bias:{}".format(self.w, self.bias)
         return s
 
     def forward(self, x):
         """x是列向量,x一定是二维的，即是只有一个向量"""
-        """x增加偏置项"""
-        if debug:
-            print("x:shape[{}]".format(x.shape))
-        self.x = np.row_stack((x, np.ones(x[0].shape, dtype=x.dtype)))
-        if debug:
+        self.x = x
+        if debug == 2:
             print("self.x:shape[{}]".format(self.x.shape))
+            print("w:shape[{}]".format(self.w.shape))
         if len(x.shape) <= 1:
             print("x input is illegal; x must be a matrix, column is feature vector")
             exit(1)
-        dot_mul = np.dot(self.w, self.x)
-        ret = self.activate.forward(dot_mul)
+        linear_res = np.dot(self.w, self.x) + self.bias
+        ret = self.activate.forward(linear_res)
         return ret
 
     def bprob(self, G):
-        """G是外部输入的对layer输出的梯度"""
+        """G是外部输入的对layer输出的梯度, 是一个一维列向量"""
         """将梯度传递给输入层，即对输入的梯度"""
         """去掉对偏置项的梯度"""
         x = self.x
-        acv_i = np.dot(self.w, x)
-        print("acv_i:shape[{}]".format(acv_i.shape))
-        print("G:shape[{}]".format(G.shape))
+        acv_i = np.dot(self.w, x) + self.bias
+        if debug == 2:
+            print("acv_i:shape[{}]".format(acv_i.shape))
+            print("G:shape[{}]".format(G.shape))
         act_g = self.activate.bprob(acv_i.T.ravel(), G.T.ravel())
-        if debug:
+        if debug == 2:
             print("w:shape[{}]".format(self.w.shape))
             print("x:shape[{}]".format(x.shape))
             print("acv_i:shape[{}]".format(acv_i.shape))
@@ -72,17 +72,18 @@ class Layer:
         tmp_G = act_g.reshape(G.shape)
         #点积
         ret_G = np.dot(self.w.T, tmp_G)
-        return ret_G[:-1]
+        return ret_G
 
     def update_G(self, G):
         """更新自己的参数"""
         x = self.x
-        acv_i = np.dot(self.w, x)
+        acv_i = np.dot(self.w, x) + self.bias
         ##激活函数输出的梯度
         act_g = self.activate.bprob(acv_i.T.ravel(), G.T.ravel())
         tmp_G = act_g.reshape(G.shape)
         w_g = np.dot(tmp_G, x.T)
         self.w = self.w - (w_g * self.lam)
+        self.bias = self.bias - (tmp_G * self.lam)
 
 class CrossEntropyLoss:
 
@@ -107,13 +108,13 @@ class DNN:
         self.total_unit_num = 0
         self.loss = CrossEntropyLoss()
 
-    def add_layer(self, unit_num, input_num = None, activation = sigmoid):
-        if input_num == None and len(self.layers) == 0:
+    def add_layer(self, unit_num, input_dimention = None, activation = sigmoid):
+        if input_dimention == None and len(self.layers) == 0:
             raise Exception("the first layer you must give input featuren_num")
-        if input_num == None:
-            input_num = self.layers[-1].unit_num
+        if input_dimention == None:
+            input_dimention = self.layers[-1].unit_num
 
-        layer = Layer(unit_num, input_num, activation)
+        layer = Layer(unit_num, input_dimention, activation)
         self.layers.append(layer)
         self.total_unit_num += unit_num
 
@@ -122,12 +123,12 @@ class DNN:
         self.train_label = label
 
     def forward(self, x):
-        """x是列向量，需要增加偏置项"""
+        """x是列向量，wx+b"""
         global debug
         tmp_out = x
         i = 0
         for layer in self.layers:
-            if debug:
+            if debug == 2:
                 print("fw layer[{0}]\ninput:\n{1}".format(i, tmp_out.ravel()))
                 i+=1
             tmp_out = layer.forward(tmp_out)
@@ -138,7 +139,7 @@ class DNN:
         la = len(self.layers)
         for layer in self.layers[::-1]:
             la -= 1
-            if debug:
+            if debug == 2:
                 print("layer[{}]:bp_degrade[shape:{}]\n{}".format(la,\
                         tmp_out.shape, tmp_out.ravel()))
             bp = layer.bprob(tmp_out)
@@ -153,15 +154,18 @@ class DNN:
         
     def forward_one(self, idx):
         global debug
-        x = np.array(self.train_data[idx]).reshape((1,4)).T
+        dim = len(self.train_data[idx].flatten(order="C"))
+        x = np.array(self.train_data[idx]).reshape((1,dim)).T
+        if debug == 2:
+            print("forward one, x:{}".format(x))
         fw_ret = self.forward(x)
-        if debug:
+        if debug == 2:
             print("fw_ret:shape[{}]".format(fw_ret.shape))
             print("fw_ret:{}".format(fw_ret.ravel()))
             print("label:{}".format(self.train_label[idx].ravel()))
         loss = self.loss.loss(self.train_label[idx], fw_ret)
         bp = self.loss.jacbi(self.train_label[idx], fw_ret)
-        if debug:
+        if debug == 2:
             print("loss:{}".format(loss))
             print("bp:{}".format(bp.ravel()))
         return [loss, bp]
@@ -169,12 +173,12 @@ class DNN:
     def fit(self, epoch, batch):
         """默认使用交叉熵损失函数"""
         size = len(self.train_data)
-        if debug:
+        if debug == 1:
             print("===== init ======")
-            self.print()
+            #self.print()
             print("+++++++++++++")
         for i in range(epoch):
-            if debug:
+            if debug == 1:
                 print("======epoch[{}]=======".format(i))
             times = size // batch
             for ts in range(times):
@@ -187,7 +191,8 @@ class DNN:
                     if idx >= size:
                         break
                     loss, bp = self.forward_one(idx)
-                    print("first idx:{}; loss:{}".format(idx, loss))
+                    if debug == 1:
+                        print("first idx:{}; loss:{}".format(idx, loss))
                     loss_sum += loss
                     bp_sum += bp
                     count += 1
@@ -197,7 +202,7 @@ class DNN:
                 loss, bp = self.forward_one(idx_base)
                 print("sec idx:{}; loss:{}".format(idx_base, loss))
                 break
-            if debug:
+            if debug == 2:
                 self.print()
                 print("+++++++++++")
 
@@ -230,9 +235,11 @@ def precission(label, predict):
 
 if __name__ == "__main__":
     train_data, train_label, test_data, test_label = load_data()
+    print("label_shape:{}".format(train_label[0]))
+    print("train_data shape:{}".format(train_data.shape))
     dnn = DNN()
-    dnn.add_layer(unit_num = 20, input_num = 4)
-    dnn.add_layer(unit_num = 4, activation = softmax)
+    dnn.add_layer(unit_num = 20, input_dimention = 4)
+    dnn.add_layer(unit_num = 3, activation = softmax)
     dnn.set_train(train_data, train_label)
     dnn.fit(epoch = 10, batch = 10)
     exit(0)
